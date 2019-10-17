baefd1db9d81f52c45ad69714f3a5ac03977ab8f
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-10-17 19:19:25 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan36-TargetSaldoTabungan/src/if1/pkg10118008/latihan36/targetsaldotabungan/IF110118008Latihan36TargetSaldoTabungan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118008.latihan36.targetsaldotabungan;
+
+/**
+ *
+* @author
+ * NAMA			: Michael Nagaku Milenn Salim
+ * KELAS		: IF-1
+ * NIM			: 10118008
+ * Deskripsi Program	: program menghitung target saldo
+ */
+public class IF110118008Latihan36TargetSaldoTabungan {
+	 public static void main(String[] args) {
+		Tabungan tabungan = new Tabungan(3500000, 6000000, 0.08);
+		tabungan.hitung();
+	}
+}
